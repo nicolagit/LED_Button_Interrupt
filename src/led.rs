b68bc199d2ff5e59989b35346bb61166ed38cc9c
Nicola <nyc.zyp@gmail.com>
2025-10-16 22:25:03 +0100

    7.68 - Exercise Implementation Part 3 - b

--- src/led.rs
+++ src/led.rs
@@ -1,5 +1,21 @@
+use core::ptr;
+
+unsafe fn read_register(addr: *mut u32) -> U32 {
+    ptr::read_volatile(addr)
+}
+
+unsafe fn write_register(addr: *mut u32, value: u32) {
+    ptr::write_volatile(addr, value)
+}
+
 pub fn led_init(port: u32, pin: u32) {
     //1. Set the gpio pin mode = output mode
+    let offset = 0;
+    let gpio_mode_register_addr = (port + offset) as *mut u32;
+    unsafe {
+        let gpio_mode_register_value = read_register(gpio_mode_register_addr);
+    }
+
     //2. Set the output type = pushpull
     //3. Set the output speed (optional)
 }
