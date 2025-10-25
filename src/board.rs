bbdac01aedefc9ad4ab976b8f258df759b2b87fa
Nicola <nyc.zyp@gmail.com>
2025-10-25 22:07:08 +0100

    7.72 - Exercise Implementation Part 7 and Testing LED module

--- src/board.rs
+++ src/board.rs
@@ -1,4 +1,4 @@
 use crate::mcu::*;
 
-pub const GREEN_LED_PIN: u32 = GPIO_PIN_0;
-pub const GREEN_LED_PORT: u32 = GPIOA_BASE;
+pub const GREEN_LED_PIN: u32 = GPIO_PIN_3;
+pub const GREEN_LED_PORT: u32 = GPIOB_BASE;
--- src/mcu.rs
+++ src/mcu.rs
@@ -1,5 +1,9 @@
 pub const GPIOA_BASE: u32 = 0x4800_0000;
 pub const GPIOB_BASE: u32 = GPIOA_BASE + 0x400; //0x4800_0400;
+
 pub const GPIO_PIN_0: u32 = 0;
+pub const GPIO_PIN_1: u32 = 1;
+pub const GPIO_PIN_2: u32 = 2;
+pub const GPIO_PIN_3: u32 = 3; // Green LED LD1 is on PB3
 
 pub const RCC_BASE: u32 = 0x4002_1000;
